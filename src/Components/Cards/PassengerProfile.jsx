8bb6cc04d6e7a21ed02ac90b360ff9025148d7e3
Mathieu <ceran.mete@gmail.com>
2024-07-11 20:11:08 +0200

    chore: Update PassengerProfile component with OrnementLeft and OrnementRight components

--- src/Components/Cards/PassengerProfile.jsx
+++ src/Components/Cards/PassengerProfile.jsx
@@ -11,6 +11,8 @@ import passengerDataEN from "../../Data/passengerEN.json";
 import passengerDataFR from "../../Data/passengerFR.json";
 import DefaultPicture from "../../assets/default.png";
 import Ornement from "../Ornement";
+import OrnementLeft from "../OrnementLeft";
+import OrnementRight from "../OrnementRight";
 
 const PassengerProfile = () => {
   const { id } = useParams();
@@ -68,57 +70,63 @@ const PassengerProfile = () => {
 
   return (
     <div className="p-4">
-      <header className="head flex justify-between items-center gap-4">
-        <Link to="/" className="text-2xl font-bold">
-          <button className="border rounded-full w-32 p-2 text-center transition-all bg-white ripple">
-            <i className="fa-solid fa-arrow-left text-black"></i>
-          </button>
-        </Link>
-        <div className="language flex gap-6">
-          <Popover placement="bottom-end">
-            <PopoverHandler>
-              <button>
-                <div className="fr border rounded-full bg-white w-[30px] h-[30px] flex items-center justify-center p-[30px]">
-                  <p className="text-black">{language}</p>
+      <OrnementLeft />
+      <OrnementRight />
+      <div className="flex justify-center mt-10">
+        <header className=" flex justify-between items-center gap-4">
+          <Link to="/" className="text-2xl font-bold">
+            <button className="border rounded-full w-32 p-2 text-center transition-all bg-white ripple">
+              <i className="fa-solid fa-arrow-left text-black"></i>
+            </button>
+          </Link>
+          <div className="language flex gap-6">
+            <Popover placement="bottom-end">
+              <PopoverHandler>
+                <button>
+                  <div className="fr border rounded-full bg-white w-[30px] h-[30px] flex items-center justify-center p-[30px]">
+                    <p className="text-black">{language}</p>
+                  </div>
+                </button>
+              </PopoverHandler>
+              <PopoverContent className="w-72 pb-0">
+                <div
+                  onClick={() => changeLanguage("FR")}
+                  className="mb-4 flex items-center gap-4 border-b border-blue-gray-50 pb-4 cursor-pointer"
+                >
+                  <div className="fr border rounded-full bg-[#0d1625] w-[30px] h-[30px] flex items-center justify-center">
+                    <p className="text-white">FR</p>
+                  </div>
+                  <div>
+                    <Typography variant="h6" color="blue-gray">
+                      Français
+                    </Typography>
+                  </div>
                 </div>
-              </button>
-            </PopoverHandler>
-            <PopoverContent className="w-72 pb-0">
-              <div
-                onClick={() => changeLanguage("FR")}
-                className="mb-4 flex items-center gap-4 border-b border-blue-gray-50 pb-4 cursor-pointer"
-              >
-                <div className="fr border rounded-full bg-[#0d1625] w-[30px] h-[30px] flex items-center justify-center">
-                  <p className="text-white">FR</p>
+                <div
+                  onClick={() => changeLanguage("EN")}
+                  className="flex items-center gap-4 border-b border-blue-gray-50 pb-4 cursor-pointer"
+                >
+                  <div className="fr border rounded-full bg-[#0d1625] w-[30px] h-[30px] flex items-center justify-center">
+                    <p className="text-white">EN</p>
+                  </div>
+                  <div>
+                    <Typography variant="h6" color="blue-gray">
+                      English
+                    </Typography>
+                  </div>
                 </div>
-                <div>
-                  <Typography variant="h6" color="blue-gray">
-                    Français
-                  </Typography>
-                </div>
-              </div>
-              <div
-                onClick={() => changeLanguage("EN")}
-                className="flex items-center gap-4 border-b border-blue-gray-50 pb-4 cursor-pointer"
-              >
-                <div className="fr border rounded-full bg-[#0d1625] w-[30px] h-[30px] flex items-center justify-center">
-                  <p className="text-white">EN</p>
-                </div>
-                <div>
-                  <Typography variant="h6" color="blue-gray">
-                    English
-                  </Typography>
-                </div>
-              </div>
-            </PopoverContent>
-          </Popover>
-        </div>
-      </header>
-      <div className="separator border mt-6"></div>
+              </PopoverContent>
+            </Popover>
+          </div>
+        </header>
+      </div>
+      <div className="flex justify-center">
+        <div className="separator border mt-6 w-[80%]"></div>
+      </div>
       <main {...handlers}>
         <div className="flex flex-col justify-center items-center">
           {passenger ? (
-            <div className="passenger-profile flex flex-col justify-center items-center gap-5 p-6 sm:max-w-full md:max-w-full lg:max-w-[80%]">
+            <div className="passenger-profile flex flex-col justify-center items-center gap-5 p-6 sm:max-w-[80%] md:max-w-[80%] lg:max-w-[70%]">
               <div className="flex gap-10 w-full justify-between items-center">
                 <button
                   onClick={() =>
