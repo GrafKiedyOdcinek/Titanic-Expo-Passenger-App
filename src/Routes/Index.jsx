f273c9ff41769826ab5d555a99f2fc6ec4b18f71
Mathieu <ceran.mete@gmail.com>
2024-07-11 21:42:25 +0200

    chore: Remove commented out code for PageTransition in Index.jsx

--- src/Routes/Index.jsx
+++ src/Routes/Index.jsx
@@ -1,18 +1,18 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import App from "../App";
 import PassengerProfile from "../Components/Cards/PassengerProfile";
-import PageTransition from "./PageTransition";
+// import PageTransition from "./PageTransition";
 
 const Index = () => {
   return (
     <BrowserRouter>
-      <PageTransition>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="*" element={<Navigate to="/" />} />
-          <Route path="/passenger/:id" element={<PassengerProfile />} />
-        </Routes>
-      </PageTransition>
+      {/* <PageTransition> */}
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/passenger/:id" element={<PassengerProfile />} />
+      </Routes>
+      {/* </PageTransition> */}
     </BrowserRouter>
   );
 };
